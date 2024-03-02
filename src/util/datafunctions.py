53473098a600228c71436107656873504f07e475
Anton <anton.zhitomirsky@gmail.com>
2024-03-02 19:44:14 +0000

    strange model behaviour

---
+++ src/util/datafunctions.py
@@ -0,0 +1,58 @@
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+def generate_data_splits(train_data):
+    # The stratify parameter makes a split so that the proportion of values in the sample produced will be the same as the proportion of values provided to parameter stratify. 
+
+    X_train, X_val, y_train, y_val = train_test_split(train_data[['text', 'keyword', 'country_code']]
+                                                    , train_data['label']
+                                                    , train_size=0.8
+                                                    , test_size=0.2
+                                                    , random_state=0
+                                                    , stratify=train_data[['label']]
+                                                    )
+   
+    return X_train, X_val, y_train, y_val
+
+
+def pie_data_distributions(pie_series, pie_series_label):
+    """Plots some pie charts for data distribution (Assume len(pie_series) inputs)
+
+    Args:
+        pie_series (_type_): pandas series 
+        pie_series_label (_type_): for the title
+    """
+
+    assert len(pie_series) == len(pie_series_label) and "expected inputs of same length"
+
+    _, axs = plt.subplots(1, len(pie_series), figsize=(len(pie_series)*5, 5))
+
+    for idx, (series, title) in enumerate(zip(pie_series, pie_series_label)):
+        tmp = series.value_counts()
+
+        wedges, _, _ = axs[idx].pie(tmp, labels=tmp.index, autopct='%1.1f%%', startangle=90)
+        legend_labels = [f'{label} ({count})' for label, count in zip(tmp.index, tmp)]
+        axs[idx].legend(wedges, legend_labels, title="Class 1 and 0", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+        axs[idx].set_title(f'Distribution for {title}')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_data_splits(X_train, X_val, y_train, y_val, test_data):
+    print("=== DISTRIBUTION FOR TRAINING DATA ===")
+    pie_data_distributions(
+        [X_train['country_code'], X_train['keyword'], y_train],
+        ['train country_code label', 'train keyword label', 'train binary classification']
+    )
+
+    print("=== DISTRIBUTION FOR VALIDATION DATA ===")
+    pie_data_distributions(
+        [X_val['country_code'], X_val['keyword'], y_val],
+        ['validation country_code label', 'validation keyword label', 'validation binary classification']
+    )
+
+    print("=== DISTRIBUTION FOR TEST DATA ===")
+    pie_data_distributions(
+        [test_data['country_code'], test_data['keyword']],
+        ['test country_code label', 'test keyword label']
+    )
---
+++ src/util/evaluation.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def performance_metrics(trainer, dataloader):
+    predicted = []
+    expected = []
+
+    predictions = trainer.predict(dataloader)
+    predictions = predictions.label_ids
+    
+    expected = np.array(dataloader.data['label'])
+    expected = expected.astype(int)
+
+    print(classification_report(expected, predictions))
+
+    set(predicted)
+    cm = confusion_matrix(expected, predictions, labels=[0, 1]) 
+
+    ax= plt.subplot()
+    sns.heatmap(cm, annot=True, fmt='g', ax=ax, cmap=sns.color_palette("ch:s=-.2,r=.6", as_cmap=True));  #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+    # labels, title and ticks
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+
+    ax.set_title('Confusion Matrix')
+
+    ax.xaxis.set_ticklabels(['Not Patronizing', 'Patronizing'])
+    ax.yaxis.set_ticklabels(['Not Patronizing', 'Patronizing'])
+
+    plt.show()
+
+
+# def model_prediction(model):
+#     """Given a model and input for the forward pass, will output pretty-printed output"""
+#     # Get one random sample from the test data
+#     random_index = X_val.sample(n=1).index.values[0]
+
+#     while random_index not in train_data.index:
+#         random_index = X_val.sample(n=1).index.values[0]
+
+#     # Extract random sample
+#     tdf = train_data.loc[[random_index]][['text', 'label']]
+#     test_paragraph, real_label = tdf['text'].values[0], tdf['label'].values[0]
+
+#     # Get model prediction
+#     predicted_label, _ = model.predict([tdf['text'].values[0]])
+
+#     print(f"=== Paragraph example ===\n{test_paragraph.capitalize()}")
+#     print(f"\tReal Label:      {real_label}")
+#     print(f"\tModel Prediction:{predicted_label[0]}")
